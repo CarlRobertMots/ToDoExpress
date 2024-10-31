4fbc020c068e2b5617d64777a4d4f82f8eeba3a1
Carl-Robert <robertm6ts@gmail.com>
2024-10-31 13:10:31 +0200

    Read data from file

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require('express')
 const app = express()
+const fs = require('node:fs')
 
 const path = require('path')
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
 app.get('/', (req, res) => {
-    res.render('index')
+    fs.readFile('/tasks', 'utf-8', (error, data) => {
+        if (error) {
+            console.error(error)
+            return
+        }
+        const tasks = data.split('\n')
+        res.render('index',{tasks:tasks} )
+    })  
+    
 
 })
 
