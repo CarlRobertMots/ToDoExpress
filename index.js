a7f0f841e98e9c9762b4cdbd8c327713045538b0
Carl-Robert <robertm6ts@gmail.com>
2024-11-22 13:14:32 +0200

    Add error handling

--- index.js
+++ index.js
@@ -33,13 +33,27 @@ const writeFile = (filename, data) => {
 app.get('/', (req,res) => {
     readFile ('./tasks.json')
     .then(tasks => {
-    res.render('index', {tasks: tasks})
+    res.render('index', {
+        tasks: tasks,
+        error: null
+        })
     })
 })
 
 app.use(express.urlencoded({extended:true}))
 
 app.post('/', (req, res) => {
+    let error = null
+    if (req.body.task.trim().length == 0) {
+        error = 'Please insert correct task data'
+        readFile('./tasks.json')
+        .then(tasks => {
+            res.render('index', {
+                tasks: tasks,
+                error: error
+            })
+        })
+    } else
     // task lists data from file
     readFile('./tasks.json',)
         .then((tasks) => {
